2074bb761ca6075637c623f8590f50f08f999aad
rafael.soares <rafael.soares@t2software.com.br>
2017-04-17 20:39:24 -0300

    Foram adicionados as funções delete e readonly nos campos e tabs

--- angular1/app/commom/components/field.js
+++ angular1/app/commom/components/field.js
@@ -7,6 +7,7 @@
       placeholder: '@',
       type: '@',
       model: '=',
+      readonly: '<',
     },
     controller: [
         'gridSystem',
@@ -18,7 +19,7 @@
     <div class="{{ $ctrl.gridClasses }}">
       <div class="form-group">
         <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
-        <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" type="{{ $ctrl.type }}" ng-model="$ctrl.model" />
+        <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" type="{{ $ctrl.type }}" ng-model="$ctrl.model" ng-readonly="$ctrl.readonly" />
       </div>
     </div>
     `
