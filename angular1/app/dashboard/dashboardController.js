23e96e718ac699bde3d649cd2964bdb46c5e99b4
rafael.soares <rafael.soares@t2software.com.br>
2017-04-17 16:48:46 -0300

    Criação do dashboard

---
+++ angular1/app/dashboard/dashboardController.js
@@ -0,0 +1,21 @@
+(function() {
+    angular.module('primeiraApp').controller('DashboardCtrl', [
+        '$http',
+        DashBoardController
+    ])
+
+    function DashBoardController($http) {
+        const vm = this
+        vm.getSummary = function() {
+            const url = 'http://localhost:3003/api/billingSummary'
+            $http.get(url).then(function(response) {
+                const { credit = 0, debt = 0 } = response.data
+                vm.credit = credit
+                vm.debt = debt
+                vm.total = credit - debt
+            })
+        }
+
+        vm.getSummary()
+    }
+})()
